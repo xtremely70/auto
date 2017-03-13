2999e6078df893ca91b46963e92d77260507ed8a
xavier <xtremely70@hanmail.net>
2017-03-13 16:08:53 +0900

    C# sample code

--- kw/kw.py
+++ kw/kw.py
@@ -27,24 +27,6 @@ class MyWindow(QMainWindow):
         elif nErrCode < 0:  # error
             self.text_edit.append("로그인 실패")
 
-"""
-        btn1 = QPushButton("Log In", self)
-        btn1.move(20, 20)
-        btn1.clicked.connect(self.btn_login)
-
-        btn2 = QPushButton("Check status", self)
-        btn2.move(20, 70)
-        btn2.clicked.connect(self.btn2_clicked)
-
-    def btn_login(self):
-        ret = self.kiwoom.dynamicCall("CommConnect()")
-
-    def btn2_clicked(self):
-        if self.kiwoom.dynamicCall("GetConnectState()") == 1:
-            self.statusBar().showMessage("Connected")
-        else:
-            self.statusBar().showMessage("Not connected")
-"""
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
