8e7a3334b64b11cff444986cee168aafc920989d
xavier <xtremely70@hanmail.net>
2017-03-15 08:25:16 +0900

    btn clicked event
    btn click event로 getChartData() 시작하도록 변경

--- kw/getChartData.py
+++ kw/getChartData.py
@@ -20,11 +20,16 @@ class MyWindow(QMainWindow):
         self.text_edit.setGeometry(10, 60, 280, 80)
         self.text_edit.setEnabled(False)
 
+        btn1 = QPushButton("getChartData", self)
+        btn1.move(190, 10)
+        btn1.clicked.connect(self.getChartData)
+
         # void OnEventConnect 통신 연결상태 변경시 이벤트
         self.kiwoom.OnEventConnect.connect(self.event_connect)
         self.kiwoom.OnReceiveTrData.connect(self.receive_trdata)
 
-        self.getChartData("032860", 10)
+        # print("Calling getChartData()...")
+        # self.getChartData("032860", 10)
 
     def event_connect(self, nErrCode):
         if nErrCode == 0:
@@ -34,8 +39,9 @@ class MyWindow(QMainWindow):
             self.text_edit.append("로그인 실패")
         self.text_edit.append(self.get_user_account)
 
-    def getChartData(self, index, interval=10):
+    def getChartData(self, index="032860", interval=10):
         # InputValue 셋업
+        print("Running getChartData", index, interval)
         self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", index)
         self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "틱범위", str(interval))
         self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "수정주가구분", "1")
